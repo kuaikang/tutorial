e8ec7471efcb4a439226bac1d8e6321a7c70bd71
kuaikang <359405466@qq.com>
2018-04-11 22:39:07 +0800

    Initial commit

---
+++ tutorial/spiders/quotes_spider.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class QuotesSpiderSpider(scrapy.Spider):
+    name = 'quotes_spider'
+    allowed_domains = ['quotes.toscrape.com']
+    start_urls = ['http://quotes.toscrape.com/']
+
+    # 必须返回一个可迭代的请求
+    def start_requests(self):
+        urls = [
+            'http://quotes.toscrape.com/page/1/',
+            'http://quotes.toscrape.com/page/2/',
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse())
+
+    # 处理请求返回的响应
+    def parse(self, response):
+        pass
